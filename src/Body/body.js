8d647ad91a243af7f9c4f53d6dff12e35da49ff1
SIDTK365 <siddharthsingh330@yahoo.in>
2021-11-10 21:46:50 +0530

    Added some UI Changes

--- src/Body/body.js
+++ src/Body/body.js
@@ -54,15 +54,15 @@ function Header2() {
                         </div>
                         <div className="hidden md:block">
                             <div class="p-2 flex">
-                                <form class="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
+                                <form class="flex flex-col md:flex-row w-3/4 md:w-full max-w-4xl md:space-x-3 space-y-3 md:space-y-0 justify-center">
                                     <div class=" relative ">
-                                        <input type="text" id="&quot;form-subscribe-Search" class="rounded-lg border-gray-400 bg-gray-800 flex-1 appearance-none border w-full py-2 px-4 bg-white text-gray-200 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Specify Subreddit r/" onChange={getVal} autocomplete="off" />
+                                        <input type="text" id="&quot;form-subscribe-Search" class="rounded-lg border-gray-400 bg-gray-800 flex-1 appearance-none border w-full py-2 px-4 bg-white text-gray-200 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Subreddit without r/" onChange={getVal} autocomplete="off" />
                                     </div>
                                     <div class="button-point flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit" onClick={assignVal}>
-                                        Search
+                                        Search Subreddit
                                     </div>
                                     <div class="button-point flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit" onClick={resetVal}>
-                                        Reset
+                                        Random Memes
                                     </div>
                                 </form>
                             </div>
@@ -129,13 +129,13 @@ function Header2() {
                                 <div class="px-2 py-6 flex justify-center">
                                     <form class="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                                         <div class=" relative ">
-                                            <input type="text" id="&quot;form-subscribe-Search" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-400 bg-gray-800 w-full py-2 px-4 text-gray-200 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Search subreddit r/" onChange={getVal} autocomplete="off" />
+                                            <input type="text" id="&quot;form-subscribe-Search" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-400 bg-gray-800 w-full py-2 px-4 text-gray-200 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Subreddit without r/" onChange={getVal} autocomplete="off" />
                                         </div>
                                         <div class="text-center flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" onClick={() => { assignVal(); setIsOpen(!isOpen); }} type="submit">
-                                            Search
+                                        Search Subreddit
                                         </div>
                                         <div class="text-center flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" onClick={() => { resetVal(); setIsOpen(!isOpen); }} type="submit">
-                                            Reset
+                                        Random Memes
                                         </div>
                                     </form>
                                 </div>
